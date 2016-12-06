bf9494853d48bfab1acebc9cc4e45f6df0111996
jespinas <janella.espinas@gmail.com>
2016-12-06 21:18:42 +1300

    Hello world zzz

---
+++ src/Dockore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dockore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
